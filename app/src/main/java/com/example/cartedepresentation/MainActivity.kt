344ce9277ccaab42ffc7c03159fd88dfc203ed23
Fabien Laurette <fabien.laurette@bnpparibas.com>
2026-05-07 11:30:32 +0200

    feat: fillMaxWidth

--- app/src/main/java/com/example/cartedepresentation/MainActivity.kt
+++ app/src/main/java/com/example/cartedepresentation/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -38,7 +39,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     Surface(color = Color.Yellow) {
         Text(
             text = "Hi, my name is  $name!",
-            modifier = modifier.padding(24.dp)
+            modifier = modifier.padding(24.dp).fillMaxWidth()
         )
     }
 }
