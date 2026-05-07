8e16238fe99e2c00a743b01314e9ceb229d9c463
Fabien Laurette <fabien.laurette@bnpparibas.com>
2026-05-07 17:22:44 +0200

    feat: ajout image

--- app/src/main/java/com/example/cartedepresentation/MainActivity.kt
+++ app/src/main/java/com/example/cartedepresentation/MainActivity.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -13,6 +16,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.cartedepresentation.ui.theme.CarteDePrésentationTheme
@@ -24,10 +28,13 @@ class MainActivity : ComponentActivity() {
         setContent {
             CarteDePrésentationTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Column() {
+                        Greeting(
+                            name = "Fabien",
+                            modifier = Modifier.padding(innerPadding)
+                        )
+                        GreetingImage(modifier = Modifier)
+                    }
                 }
             }
         }
@@ -37,9 +44,28 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Surface(color = Color.Yellow) {
+            Text(
+                text = "Hi, my name is  $name!",
+                modifier = modifier
+                    .padding(24.dp)
+                    .fillMaxWidth()
+            )
+    }
+}
+
+@Composable
+fun GreetingImage(modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.androidparty)
+    Box(modifier) {
+        Image(
+            painter = image,
+            contentDescription = null
+        )
         Text(
-            text = "Hi, my name is  $name!",
-            modifier = modifier.padding(24.dp).fillMaxWidth()
+            text = "Hello!",
+            modifier = modifier
+                .padding(24.dp)
+                .fillMaxWidth()
         )
     }
 }
@@ -50,4 +76,10 @@ fun GreetingPreview() {
     CarteDePrésentationTheme {
         Greeting("Fabien")
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingImagePreview() {
+    GreetingImage()
 }
